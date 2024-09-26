086e321aea3dccc5989d909fd625f3e19abc34e5
Fernando A. <fernandoach333@gmail.com>
2024-09-26 09:02:57 -0500

    OK: static tat content

--- src/components/progress/cardProgress.jsx
+++ src/components/progress/cardProgress.jsx
@@ -2,14 +2,14 @@ import { Button, Card, CardBody, CardFooter, Chip, CircularProgress, Link } from
 
 function CardProgress ({ progress, title, href }) {
   return (
-    <Card className="w-[300px] h-[300px] border border-success  bg-background/30 shadow-md rounded-md p-4 text-center">
+    <Card className="w-[300px] h-[300px] border border-foreground  bg-background/30 shadow-md rounded-md p-4 text-center">
       <CardBody className="justify-center items-center pb-0">
         <CircularProgress
           classNames={{
             svg: 'w-36 h-36 drop-shadow-md',
-            indicator: 'stroke-success',
-            track: 'stroke-success/20',
-            value: 'text-3xl font-semibold text-success'
+            indicator: 'stroke-foreground',
+            track: 'stroke-foreground/20',
+            value: 'text-3xl font-semibold text-foreground'
           }}
           value={progress * 100 / 21}
           strokeWidth={4}
@@ -20,8 +20,8 @@ function CardProgress ({ progress, title, href }) {
       <CardFooter className=" flex flex-col justify-center items-center pt-0">
         <Chip
           classNames={{
-            base: 'border-1 border-success/30',
-            content: 'text-success/90 text-small font-semibold'
+            base: 'border-1 border-foreground/30',
+            content: 'text-foreground/90 text-small font-semibold'
           }}
           aria-labelledby={ title }
           variant="bordered"
@@ -29,9 +29,9 @@ function CardProgress ({ progress, title, href }) {
           { title }
         </Chip>
         {
-          progress * 100 / 21 <= 100
-            ? <Button as={Link} variant='ghost' color='success' className='mt-4' href={href} aria-labelledby='Continuar'>Continuar</Button>
-            : <span aria-labelledby='Gracias por su participación' className='text-sm mt-4 text-success'>Gracias por su participación</span>
+          (progress * 100 / 21) < 100
+            ? <Button as={Link} variant='ghost' color='default' className='mt-4' href={href} aria-labelledby='Continuar'>Continuar</Button>
+            : <span aria-labelledby='Gracias por su participación' className='text-sm mt-4 text-foreground'>Gracias por su participación</span>
         }
       </CardFooter>
     </Card>
---
+++ src/static/tat.js
@@ -0,0 +1,146 @@
+const tatQuestions = [
+  {
+    theme: 'Un niño mirando un violín.',
+    image: '/images/tat/01.jpg'
+  },
+  {
+    theme: 'Una mujer sostiene un libro en una escena campestre y observa a un hombre trabajando en un campo al fondo.',
+    image: '/images/tat/02.jpg'
+  },
+  {
+    bm: {
+      theme: 'Un niño acurrucado junto a un sofá con un objeto ambiguo colocado a su lado, posiblemente un revólver o un juego de llaves.',
+      image: '/images/tat/03BM.jpg'
+    },
+    gf: {
+      theme: 'Una mujer parada junto a una puerta abierta sosteniendo un costado de la puerta con una mano y con la otra cubriendo su rostro abatido.',
+      image: '/images/tat/03GF.jpg'
+    }
+  },
+  {
+    theme: 'Un hombre se aleja de una mujer que lo agarra por los hombros.',
+    image: '/images/tat/04.jpg'
+  },
+  {
+    theme: 'Una mujer mirando hacia una habitación desde la puerta.',
+    image: '/images/tat/05.jpg'
+  },
+  {
+    bm: {
+      theme: 'Un hombre sostiene su sombrero con la cara hacia abajo y una mujer mayor está parada junto a él paralela a una ventana.',
+      image: '/images/tat/06BM.jpg'
+    },
+    gf: {
+      theme: 'Un hombre mayor con una pipa en la boca está hablando con una mujer más joven sentada en un sofá, quien lo mira.',
+      image: '/images/tat/06GF.jpg'
+    }
+  },
+  {
+    bm: {
+      theme: 'Un hombre más joven mirando al espacio (nada) y un hombre mayor mirándolo.',
+      image: '/images/tat/07BM.jpg'
+    },
+    gf: {
+      theme: 'Una niña está sentada en un sofá con una muñeca en sus manos y una mujer mayor sentada detrás de ella le está leyendo un libro.',
+      image: '/images/tat/07GF.jpg'
+    }
+  },
+  {
+    bm: {
+      theme: 'En primer plano se ve a un niño mirando hacia el exterior. En el fondo se ven a dos hombres operando a un paciente.',
+      image: '/images/tat/08BM.jpg'
+    },
+    gf: {
+      theme: 'Una mujer sentada en una silla con la barbilla apoyada en la mano, mirando al vacío.',
+      image: '/images/tat/08GF.jpg'
+    }
+  },
+  {
+    bm: {
+      theme: 'Cuatro hombres acostados en un campo uno frente al otro.',
+      image: '/images/tat/09BM.jpg'
+    },
+    gf: {
+      theme: 'Una mujer parada detrás de un árbol mirando a otra mujer, que parece estar corriendo en una playa.',
+      image: '/images/tat/09GF.jpg'
+    }
+  },
+  {
+    theme: 'Una persona descansa sobre el hombro de otra. Se desconoce el género.',
+    image: '/images/tat/10.jpg'
+  },
+  {
+    theme: 'Varias figuras se dirigen hacia un puente sobre una carretera en un abismo. Parece haber un dragón sobre ellas apoyado en el borde de un acantilado.',
+    image: '/images/tat/11.jpg'
+  },
+  {
+    m: {
+      theme: 'Un niño está acostado en una cama, con los ojos cerrados, y un hombre está de pie sobre el niño con su mano levantada.',
+      image: '/images/tat/12M.jpg'
+    },
+    f: {
+      theme: 'Una mujer joven al frente y una mujer mayor sosteniendo su barbilla al fondo.',
+      image: '/images/tat/12F.jpg'
+    },
+    bg: {
+      theme: 'Hay un árbol y un bote de remos a su lado en un entorno rural sin presencia de ningún ser humano.',
+      image: '/images/tat/12BG.jpg'
+    }
+  },
+  {
+    mf: {
+      theme: 'Una mujer está acostada en la cama al fondo y un hombre está de pie frente a ella con la cabeza entre los brazos.',
+      image: '/images/tat/13MF.jpg'
+    },
+    b: {
+      theme: 'Un niño sentado en la puerta de una cabaña de troncos.',
+      image: '/images/tat/13B.jpg'
+    },
+    g: {
+      theme: 'Hay un tramo de escaleras y una niña subiéndolas.',
+      image: '/images/tat/13G.jpg'
+    }
+  },
+  {
+    theme: 'Una persona se recuesta contra una ventana.',
+    image: '/images/tat/14.jpg'
+  },
+  {
+    theme: 'Un hombre está de pie con las manos entrelazadas. Hay lápidas por todas partes.',
+    image: '/images/tat/15.jpg'
+  },
+  {
+    theme: 'Tarjeta en blanco.',
+    image: '/images/tat/16.jpg'
+  },
+  {
+    bm: {
+      theme: 'Un hombre desnudo subiendo o bajando por una cuerda.',
+      image: '/images/tat/17BM.jpg'
+    },
+    gf: {
+      theme: 'El sol brilla entre las nubes y hay un edificio sobre el puente. Una mujer está parada en el puente mirando el agua.',
+      image: '/images/tat/17GF.jpg'
+    }
+  },
+  {
+    b: {
+      theme: 'Tres manos agarran a un hombre vestido con un abrigo largo.',
+      image: '/images/tat/18B.jpg'
+    },
+    gf: {
+      theme: 'Justo debajo del tramo de escaleras, una mujer está agarrando el cuello de otra mujer.',
+      image: '/images/tat/18GF.jpg'
+    }
+  },
+  {
+    theme: 'Una representación surrealista de nubes y una casa cubierta de nieve.',
+    image: '/images/tat/19.jpg'
+  },
+  {
+    theme: 'La tarjeta muestra a un hombre apoyado en un poste de luz por la noche en una atmósfera brumosa.',
+    image: '/images/tat/20.jpg'
+  }
+]
+
+export { tatQuestions }
